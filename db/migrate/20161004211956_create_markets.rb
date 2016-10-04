364216a485a832d4786246bbb96eefdf5edbc438
Rachel <rachel.l.pavilanis@gmail.com>
2016-10-04 14:31:20 -0700

    added models for vendors, markets, sales, products

---
+++ db/migrate/20161004211956_create_markets.rb
@@ -0,0 +1,15 @@
+class CreateMarkets < ActiveRecord::Migration
+  def change
+    create_table :markets do |t|
+      t.string :integer
+      t.string :name
+      t.string :address
+      t.string :city
+      t.string :county
+      t.string :state
+      t.string :zip
+
+      t.timestamps null: false
+    end
+  end
+end
