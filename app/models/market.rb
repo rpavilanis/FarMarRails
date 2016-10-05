0a7b4ba081180766b7f808276f8bcd68ae8f5c7c
Rachel <rachel.l.pavilanis@gmail.com>
2016-10-05 10:05:14 -0700

    added validations for market and vendor models

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,5 +1,9 @@
 class Market < ActiveRecord::Base
   has_many :vendors
   has_many :products
-  has_many :sales 
+  has_many :sales
+
+  validates :name, presence: true, uniqueness: true
+  validates :address, :city, :state, :zip, :county, presence: true
+
 end
